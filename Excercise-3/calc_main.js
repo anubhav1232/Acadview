9d99165309f307e134e8baa684fd70b560b11c69
Anubhav Thakur <32287141+anubhav1232@users.noreply.github.com>
2019-03-12 11:23:41 +0530

    Add files via upload
    Calculator (add,subtract,multiply,.divide)

---
+++ Excercise-3/calc_main.js
@@ -0,0 +1,6 @@
+var calc = require('./calculator');
+
+console.log(calc.multiplication(3,5));
+console.log(calc.divide(5,3));
+console.log(calc.add(3,5));
+console.log(calc.subtract(3,5));
---
+++ Excercise-3/calculator.js
@@ -0,0 +1,17 @@
+var multiplication =function(a,b){
+  return  `The product of number is ${a*b}`
+}
+var divide =function(a,b){
+  return  `The product of number is ${a/b}`
+}
+var subtract =function(a,b){
+  return  `The product of number is ${a-b}`
+}
+var add =function(a,b){
+  return  `The product of number is ${a+b}`
+}
+module.exports.multiplication = multiplication;
+
+module.exports.divide = divide;
+module.exports.subtract = subtract;
+module.exports.add = add;
